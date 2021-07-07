17809d59a2f6871ab8636946a4c6d0aca2401a43
Davef <76170782+davefdev@users.noreply.github.com>
2021-07-08 00:25:50 +0100

    corrected the search function
    I had various errors in the search bar so I altered it to:
    
    - Store the noResults element at the top, so I know what element to delete to clear any error message
    - Pass the event in the event handlers, so that I can access the searchInput value
    - Get the firstname and lastname of each potential match inside of the loop
    - The logic to display the results needs to happen AFTER the loop, not inside the loop (need to check all matches before displaying anything)

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 
 console.log(data);
-
+ 
 /*
 For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
-
+ 
 // Global Variables
 const page = document.querySelector('.page');
 const itemsPerPage = 9;
@@ -18,11 +18,12 @@ const studentListUl = document.querySelector('.student-list');
 const studentItemLi = studentListUl.children;
 const pagBtnUL = document.createElement('ul');
 const pagDiv = document.createElement('div');
+let noResults
 pagDiv.className = 'pagination';
-
+ 
 console.log(studentListUl);
 console.log(studentItemLi);
-
+ 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
@@ -33,7 +34,7 @@ const showPage = (list, page) => {
    let lastIndex = page * itemsPerPage; 
    //Set student list UL to empty string to remove students prev displayed
    studentListUl.innerHTML = '';
-
+ 
    //For loop to go through each student entry once
    for (let i = 0; i < list.length; i++) {
       //Conditional statement to check if current position should be displayed
@@ -54,38 +55,15 @@ const showPage = (list, page) => {
       }
    }
 }
-
+ 
 //test showPage function is working
 showPage(data, 1);
-
-
-/*const showPage = (list, page) => {   
-   //Create first and last student variables with math calculation
-   let firstIndex = (page * itemsPerPage) - itemsPerPage;
-   let lastIndex = page * itemsPerPage; 
-   //Set student list UL to empty string to remove students prev displayed
-   studentListUl.innerHTML = '';
-
-   //For loop to go through each student entry once
-   for (let i = 0; i < list.length; i++) {
-      //Conditional statement to check if current position should be displayed
-      if (i >= firstIndex && i < lastIndex) {
-         //make the students display value appear on the page
-         list[i].style.display = 'block';
-      } else {
-         //or it does not if not meet condition
-         list[i].style.display = 'none';
-      }
-   }
-}
-*/
-
-
+ 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-
+ 
 const addPagination = list => {
    //Var to store no of pag buttons, using Math.ceil and list par length divided by no of items per page
    const numOfBtns= Math.ceil(list.length/itemsPerPage);
@@ -93,7 +71,7 @@ const addPagination = list => {
    const linkList = document.querySelector('.link-list');
    //remove any prev buttons
    linkList.innerHTML = ''; 
-
+ 
    //for loop to go through each pag button
    for (let i = 1; i <= numOfBtns; i++) {
       //method to add template literal of DOM elements for button, after last child of linkList
@@ -103,17 +81,17 @@ const addPagination = list => {
          </li>`
       );   
    }
-
+ 
    //select first button element and set it's class to active
    const button = document.querySelector('button');
    button.className = 'active';
-
+ 
    //create an event listener on the page buttons 
    linkList.addEventListener('click', (e) => {
-     
+ 
       //assigns the target of the event to btn variable 
       const btn = e.target
-
+ 
       //check to ensure element clicked is a button using if statement, return early if not
       if (btn.tagName !== 'BUTTON') {
          return
@@ -121,25 +99,25 @@ const addPagination = list => {
       //remove active class from prev button
       let prevButton = document.querySelector('.active');
       prevButton.classList.remove('active');
-   
+ 
       //add active class to clicked button
       btn.classList.add('active');
-   
+ 
       //accesss the btn's page
       const page = btn.dataset.page;
-
+ 
       //call showPage func with arguments
       showPage(list, page);
    });
 }
-
-
+ 
+ 
 //*****Extra credits attempt****** 
 //Search form
 //Create and append search bar with button
 const appendSearchForm = () => {
   //declare variables for search bar
-   const pageHeader = document.getElementsByClassName('page-header');
+   const pageHeader = document.getElementsByClassName('header')[0];
    const searchDiv = document.createElement('div');
    const searchInput = document.createElement('input');
    const searchBtn = document.createElement('button');
@@ -147,66 +125,79 @@ const appendSearchForm = () => {
    searchDiv.className = 'student-search';
    searchInput.setAttribute('placeholder','Search students here');
    searchBtn.textContent = 'search';
-  
+ 
    //append search bar and button to DOM
-   pageHeader[0].appendChild(searchDiv);
    searchDiv.appendChild(searchInput);
    searchDiv.appendChild(searchBtn);
-}
-  
-//Search form function 
-const searchStudents = list => {
-
-   //put results in a new array
-   let results = [];
-
-   //make the student's name and search input upper case
-   const firstName = list[i].name.first.toUpperCase();
-   const lastName = list[i].name.last.toUpperCase();
-   const inputText = searchInput.value.toUpperCase();
-
-   //for loop to run through student array
-   for (let i = 0; i < list.length; i++) {
-
-      //If no results found, display No results message.
-      if (results.length === 0 || !inputText) {
-         //make new h2 element to put message
-         const noResults = document.createElement('h2');
-         //access textContent to display message
-         noResults.textContent = `No results found for ${searchInput.value}`;
-         //add a class value 
-         noResults.classList.add('no-results');
-         //append the message to page element
-         document.getElementsByClassName('page')[0].appendChild(noResults);
-         //add the buttons
-         addPagination(results);
-      } else {
-       
-       //if statement and includes method to check input value matches student names
-        if (firstName.includes(inputText) || lastName.includes(inputText)) {
-           //adds matches to end of results array
-           results.push(list[i]);
-        }
-         //run the showPage function with results array passed in and buttons
-           showPage(results, 1);
-           addPagination(results);
-        }
-     }
-
-   //Event listener for typing in search box, run search function (does not work with preventdefault)
-   searchInput.addEventListener('keyup', () => {
-      searchStudents(data);
+   pageHeader.appendChild(searchDiv);
+ 
+    //Event listener for typing in search box, run search function (does not work with preventdefault ac)
+    searchInput.addEventListener('keyup', (event) => {
+      searchStudents(data, event.target);
    });
-
+ 
    //Event listener for clicking on button, run search function
    searchBtn.addEventListener('click', (event) => {
       event.preventDefault();
-      searchStudents(data);
+      searchStudents(data, event.target);
    });
 }
-
+ 
+//Search form function 
+const searchStudents = (list, searchInput) => {
+   //clear old error messages
+   if (noResults) {
+      noResults.parentElement.removeChild(noResults);
+      noResults = null;
+   }
+ 
+   const inputText = searchInput.value.toUpperCase();
+ 
+   //if no search input then display all the data
+   if (!inputText) {
+      showPage(data, 1);
+      //return early
+      return
+   }
+ 
+   //put results in a new array
+   let results = [];
+ 
+   //for loop to run through student array
+   for (let i = 0; i < list.length; i++) {
+      //make the student's name and search input upper case
+      const firstName = list[i].name.first.toUpperCase();
+      const lastName = list[i].name.last.toUpperCase();
+ 
+       //if statement and includes method to check input value matches student names
+       if (firstName.includes(inputText) || lastName.includes(inputText)) {
+         //adds matches to end of results array
+         results.push(list[i]);
+      }
+   }
+ 
+   //If no results found, display No results message.
+   if (results.length === 0 || !inputText) {
+      //make new h2 element to put message
+      noResults = document.createElement('h2');
+      //access textContent to display message
+      noResults.textContent = `No results found for ${searchInput.value}`;
+      //add a class value 
+      noResults.classList.add('no-results');
+      //append the message to page element
+      document.getElementsByClassName('page')[0].appendChild(noResults);
+      //add the buttons
+      showPage(results, 1);
+      addPagination(results);
+   } else {
+      //run the showPage function with results array passed in and buttons
+      showPage(results, 1);
+      addPagination(results);
+   }
+}
+ 
 //Call all functions
-//appendSearchForm();
+appendSearchForm();
 showPage(data, 1);
 addPagination(data);
 
